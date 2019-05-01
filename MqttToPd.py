d8fd08409a0a7bb484a23f9ef7ead666be3cbdd5
Belvedersky <qwink186@icloud.com>
2019-05-01 15:05:07 +0500

    #just_test

--- MqttToPd.py
+++ MqttToPd.py
@@ -4,28 +4,29 @@ import subprocess
 import paho.mqtt.client as mqtt
 import json
 
-
-broker_address="192.168.0.73"
-mqtt_topic="sensor"
+# "192.168.88.241"
+broker_address="46.48.36.15" 
+mqtt_topic="LedOut"
 
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)   
-    #playSingleShot()
+#     print("message received " ,str(message.payload.decode("utf-8")))
+#     print("message topic=",message.topic)
+#     print("message qos=",message.qos)
+#     print("message retain flag=",message.retain)   
+#     playSingleShot()
     obj = json.loads(str(message.payload.decode("utf-8")))    
-    val = obj['v']
+#     val = obj['state']
+    val = int(message.payload)
     print("received ", val)
     send2Pd(val)
 
 
 def send2Pd(message=''):
-    os.system("echo '" + message + "' | /Applications/Pd-0.48-1.app/Contents/Resources/bin/pdsend 3000 localhost udp")
+    os.system("echo '" + message + "' | /Applications/Pd-0.49-1.app/Contents/Resources/bin/pdsend 3000 localhost udp")
 
 
 print("creating new mqtt client instance")
-client = mqtt.Client("Luke")
+client = mqtt.Client("PureData")
 
 print("connecting to broker")
 client.connect(broker_address, 1883)
